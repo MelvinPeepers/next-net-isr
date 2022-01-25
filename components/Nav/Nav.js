8da32aaff8335f3e49eb30b4ed06c6a0e96025dd
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2022-01-24 19:34:11 -0500

    Initial Commit

---
+++ components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+          </div>
+        </nav>
+    );
+}
